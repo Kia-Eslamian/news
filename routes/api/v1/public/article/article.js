d7408bbf07efc9802db092a8d6c21ae2063b12cf
Kia <kiaesl78@gmail.com>
2023-02-24 23:20:08 +0330

    categories completed

--- routes/api/v1/public/article/article.js
+++ routes/api/v1/public/article/article.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 
 // models
 const articleModel = require('../../../../../models/article/article');
+const categoryModel = require('../../../../../models/article/articleCategory');
 
 // sub routers
 const categoryRouter = require('./articleCategory/articleCategory');
@@ -17,88 +18,66 @@ router.use('/comment', commentRouter);
 router.get('/', async (req, res) => {
     try {
 
-        const articles = await articleModel.aggregate(
-            [
-                {
-                    '$lookup': {
-                        'from': 'admins',
-                        'localField': 'author',
-                        'foreignField': '_id',
-                        'as': 'author'
+        const category_id = req.query['category'];
+
+
+        const aggregationPipeline = []
+        if (category_id !== undefined) {
+            const category = await categoryModel.findById(category_id);
+            if (category) {
+                aggregationPipeline.push(
+                    {
+                        '$match': { category: category._id }
+                    },
+                    {
+                        '$lookup': {
+                            'from': 'admins',
+                            'localField': 'author',
+                            'foreignField': '_id',
+                            'as': 'author'
+                        }
+                    }, {
+                    '$unwind': {
+                        'path': '$author',
+                        'preserveNullAndEmptyArrays': true
                     }
                 }, {
-                '$unwind': {
-                    'path': '$author',
-                    'preserveNullAndEmptyArrays': true
-                }
-            }, {
-                '$lookup': {
-                    'from': 'articleCategories',
-                    'localField': 'categories',
-                    'foreignField': '_id',
-                    'as': 'categories'
-                }
-            }, {
-                '$lookup': {
-                    'from': 'comments',
-                    'localField': 'comments',
-                    'foreignField': '_id',
-                    'as': 'comments'
-                }
-            }, {
-                '$project': {
-                    'title': 1,
-                    'page_title': 1,
-                    'url': 1,
-                    'description': 1,
-                    'summary': 1,
-                    'image': 1,
-                    'author.firstName': 1,
-                    'author.lastName': 1,
-                    'categories.title': 1,
-                    'comments': 1,
-                    'createdAt': 1,
-                    'updatedAt': 1
-                }
-            }
-            ]
-        );
-
-        return res.status(200).json({
-            success: true,
-            data: {result: articles}
-        });
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success: false,
-            message: "Internal server erro"
-        });
-    }
-});
-
-
-// get single article
-router.get('/:article_id', async (req, res) => {
-    try {
-
-        const article_id = req.params['article_id'];
-
-        const article = await articleModel.findById(article_id);
-        if (!article) {
-            return res.status(400).json({
-                success: false,
-                message: "article not found"
-            });
-        }
-
-        const articles = await articleModel.aggregate(
-            [
-                {
-                    '$match': {
-                        _id: article._id
+                    '$lookup': {
+                        'from': 'articleCategories',
+                        'localField': 'categories',
+                        'foreignField': '_id',
+                        'as': 'categories'
                     }
+                }, {
+                    '$lookup': {
+                        'from': 'comments',
+                        'localField': 'comments',
+                        'foreignField': '_id',
+                        'as': 'comments'
+                    }
+                }, {
+                    '$project': {
+                        'title': 1,
+                        'page_title': 1,
+                        'url': 1,
+                        'description': 1,
+                        'summary': 1,
+                        'image': 1,
+                        'author.firstName': 1,
+                        'author.lastName': 1,
+                        'categories.title': 1,
+                        'comments': 1,
+                        'createdAt': 1,
+                        'updatedAt': 1
+                    }
+                }
+                );
+            }
+
+        } else {
+            aggregationPipeline.push(
+                {
+                    '$match': {}
                 },
                 {
                     '$lookup': {
@@ -142,12 +121,95 @@ router.get('/:article_id', async (req, res) => {
                     'updatedAt': 1
                 }
             }
+            );
+        }
+
+        const articles = await articleModel.aggregate(aggregationPipeline);
+
+        return res.status(200).json({
+            success: true,
+            data: { result: articles }
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server erro"
+        });
+    }
+});
+
+
+// get single article
+router.get('/:article_id', async (req, res) => {
+    try {
+
+        const article_id = req.params['article_id'];
+
+        const article = await articleModel.findById(article_id);
+        if (!article) {
+            return res.status(400).json({
+                success: false,
+                message: "article not found"
+            });
+        }
+
+        const articles = await articleModel.aggregate(
+            [
+                {
+                    '$match': {
+                        _id: article._id
+                    }
+                },
+                {
+                    '$lookup': {
+                        'from': 'admins',
+                        'localField': 'author',
+                        'foreignField': '_id',
+                        'as': 'author'
+                    }
+                }, {
+                    '$unwind': {
+                        'path': '$author',
+                        'preserveNullAndEmptyArrays': true
+                    }
+                }, {
+                    '$lookup': {
+                        'from': 'articleCategories',
+                        'localField': 'categories',
+                        'foreignField': '_id',
+                        'as': 'categories'
+                    }
+                }, {
+                    '$lookup': {
+                        'from': 'comments',
+                        'localField': 'comments',
+                        'foreignField': '_id',
+                        'as': 'comments'
+                    }
+                }, {
+                    '$project': {
+                        'title': 1,
+                        'page_title': 1,
+                        'url': 1,
+                        'description': 1,
+                        'summary': 1,
+                        'image': 1,
+                        'author.firstName': 1,
+                        'author.lastName': 1,
+                        'categories.title': 1,
+                        'comments': 1,
+                        'createdAt': 1,
+                        'updatedAt': 1
+                    }
+                }
             ]
         );
 
         return res.status(200).json({
             success: true,
-            data: {result: articles[0]}
+            data: { result: articles[0] }
         });
 
     } catch (error) {
--- views/public/js/home.js
+++ views/public/js/home.js
@@ -1,18 +1,67 @@
 $(document).ready(function () {
+    $('#main-container').empty();
 
     getArticleList();
 
 
+    $('#all').click(function (e) {
+        $('#main-container').empty();
+
+        e.preventDefault();
+
+        getArticleList()
+    });
+
+    $('#World').click(function (e) {
+        $('#main-container').empty();
+        e.preventDefault();
+
+        getArticleList("http://localhost:1000/api/v1/public/article?category=63f7a17dc7d6cb7e9b54d4bf")
+
+
+    });
+
+    $('#Politics').click(function (e) {
+        $('#main-container').empty();
+
+        e.preventDefault();
+
+        getArticleList("http://localhost:1000/api/v1/public/article?category=63f7a175c7d6cb7e9b54d4bd")
+    });
+
+    $('#Business').click(function (e) {
+        $('#main-container').empty();
+
+        e.preventDefault();
+
+        getArticleList("http://localhost:1000/api/v1/public/article?category=63f7a16cc7d6cb7e9b54d4bb")
+    });
+
+    $('#Science').click(function (e) {
+        $('#main-container').empty();
+
+        e.preventDefault();
+
+        getArticleList("http://localhost:1000/api/v1/public/article?category=63f7a157c7d6cb7e9b54d4b9")
+    });
+
+    $('#Art').click(function (e) {
+        $('#main-container').empty();
+
+        e.preventDefault();
+
+        getArticleList("http://localhost:1000/api/v1/public/article?category=63f7a128c7d6cb7e9b54d4b7")
+    });
 
 });
 
 
 
 
-function getArticleList() {
+function getArticleList(url = "http://localhost:1000/api/v1/public/article") {
     $.ajax({
         type: "GET",
-        url: "http://localhost:1000/api/v1/public/article",
+        url,
         success: function (response) {
             if (response.success === true) {
                 const articleList = response.data.result;
