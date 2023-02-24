825cba2d96b05e7b1a97342e2f967a09a808570e
Kia <kiaesl78@gmail.com>
2023-02-24 21:07:22 +0330

    comment completed

--- routes/api/v1/private/article/comment/comment.js
+++ routes/api/v1/private/article/comment/comment.js
@@ -10,11 +10,30 @@ const articleModel = require('../../../../../../models/article/article');
 router.get('/', async (req, res) => {
     try {
 
-        const comments = await commentModel.find({});
+        const comments = await commentModel.aggregate([
+            {
+                '$match':{
+                    status:{$ne:'TRASHED'}
+                }
+            },
+            {
+                '$lookup': {
+                    'from': 'articles',
+                    'localField': 'article',
+                    'foreignField': '_id',
+                    'as': 'article'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$article',
+                    'preserveNullAndEmptyArrays': true
+                }
+            }
+        ]);
 
         return res.status(200).json({
             success: true,
-            data: {result: comments}
+            data: { result: comments }
         });
 
     } catch (error) {
@@ -40,7 +59,7 @@ router.get('/:comment_id', async (req, res) => {
 
         return res.status(200).json({
             success: true,
-            data: {result: comment}
+            data: { result: comment }
         });
 
 
@@ -53,12 +72,11 @@ router.get('/:comment_id', async (req, res) => {
     }
 });
 
-// change comment status
-router.patch('/status/:comment_id', async (req, res) => {
+// save comment status
+router.get('/status/confirm/:comment_id', async (req, res) => {
     try {
 
         const comment_id = req.params['comment_id'];
-        const status = req.body['status'];
 
 
         const comment = await commentModel.findById(comment_id);
@@ -77,22 +95,68 @@ router.patch('/status/:comment_id', async (req, res) => {
             });
         }
 
-        if (status === 'CONFIRMED') {
-
-            comment.status === status;
-            await comment.save();
-
-            article.comments.push(comment._id);
-            await article.save();
-
-        } else if (status === "TRASHED") {
-            comment.status === status;
-            await comment.save();
+        if (article.comments.includes(comment._id)) {
+            return res.status(200).json({
+                success: true,
+                message: 'comment saved'
+            });
         }
+
+        await articleModel.findByIdAndUpdate(article._id, { $push: { comments: comment._id } });
+        await commentModel.findByIdAndUpdate(comment._id, { status: "CONFIRMED" });
 
         return res.status(200).json({
             success: true,
-            message: `وضعیت کامنت به ${status} تغییر کرد`
+            message: 'comment saved'
+        });
+
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+});
+
+
+// remove comment status
+router.get('/status/remove/:comment_id', async (req, res) => {
+    try {
+
+        const comment_id = req.params['comment_id'];
+
+
+        const comment = await commentModel.findById(comment_id);
+        if (!comment) {
+            return res.status(400).json({
+                success: false,
+                message: "کامنت یافت نشد"
+            });
+        }
+
+        const article = await articleModel.findById(comment.article);
+        if (!article) {
+            return res.status(400).json({
+                success: false,
+                message: "مقاله یافت نشد"
+            });
+        }
+
+        if (!article.comments.includes(comment._id)) {
+            return res.status(200).json({
+                success: true,
+                message: 'comment removed'
+            });
+        }
+
+        await articleModel.findByIdAndUpdate(article._id, { $pull: { comments: comment._id } });
+        await commentModel.findByIdAndUpdate(comment._id, { status: "TRASHED" });
+
+        return res.status(200).json({
+            success: true,
+            message: 'comment removed'
         });
 
 
--- views/public/js/comment.js
+++ views/public/js/comment.js
@@ -6,18 +6,18 @@ $(document).ready(function () {
 function getCommentList() {
     $.ajax({
         type: "GET",
-        url: "http://localhost:1000/api/v1/public/article/comment/",
+        url: "http://localhost:1000/api/v1/private/article/comment/",
         success: function (response) {
             if (response.success === true) {
 
-                const commentList = response.data.result
+                const commentList = response.data.result;
 
                 for (let index = 0; index < commentList.length; index++) {
                     const comment = commentList[index];
 
                     $('#comment_tbody').append(trGenerator({
                         index: index + 1,
-                        articleTitle: "articleTitle",
+                        article_id: comment.article._id,
                         commentDescription: comment.message,
                         comment_id: comment._id,
                     }));
@@ -33,24 +33,23 @@ function getCommentList() {
     });
 }
 
-function trGenerator({ articleTitle, commentDescription, comment_id, index }) {
+
+function trGenerator({ commentDescription,comment_id, article_id, index }) {
+
+    // console.log(onClickHandler);
     return `
     
     <tr>
                             <th scope="row">${index}</th>
                             <td>
-                                <a href="" class="text-decoration-none d-inline-block text-truncate" style="max-width: 150px;">
-                                ${articleTitle}
-                                </a>
-                            </td>
-                            <td>
-                                <a href="" class="text-decoration-none d-inline-block text-truncate" style="max-width: 150px;">comment
+                                <p class="text-decoration-none d-inline-block text-truncate" style="max-width: 250px;">comment
                                     ${commentDescription}
-                                </a>
+                                </p>
                             </td>
                             <td class="d-flex flex-row align-items-baseline justify-content-evenly">
-                                <button class="w-100 btn btn-outline-primary me-1">accept</button>
-                                <button class="w-100 btn btn btn-outline-danger">deny</button>
+                                <a class="w-100 btn btn-outline-primary me-1" href="http://localhost:1000/api/v1/private/article/comment/status/confirm/${comment_id}">save</a>
+                                <a class="w-100 btn btn btn-outline-danger me-1" href="http://localhost:1000/api/v1/private/article/comment/status/remove/${comment_id}">remove</a>
+                                <a class="w-100 btn btn btn-outline-secondary" href="http://localhost:1000/article/${article_id}">show</a>
                             </td>
                         </tr>
     
